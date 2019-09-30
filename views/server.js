2e6a3ea03150be971d92f35dcacd1ecf7f665dc8
Lotus Bug <torresbrittany1994@gmail.com>
2019-09-29 21:20:31 -0700

    added routes var

--- views/server.js
+++ views/server.js
@@ -4,7 +4,7 @@ var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 
 var app = express();
-app.use(express.static(_dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({
  }));
 
  app.set("view engine", "handlebars");
+
+ var routes = require("./controllers/routes.js");
+     app,use("/",routes);
 
  var port = 4000;
  app.listen(port);
